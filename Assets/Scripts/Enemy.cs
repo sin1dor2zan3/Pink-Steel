986f657ed86d034f0e1da8c046a2676e72c5e78c
sin1dor2zan3 <sin1dor2zan3@gmail.com>
2025-11-03 23:04:54 -0500

    Task 2(Sinclair): Added EnemyTwo prefab and updated GameManager spawning

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, -1, 0) * speed * Time.deltaTime);
+        if (transform.position.y < -6.5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
